4db1b8185e860636174ceb615757adf482c4c154
Carlos <clockwerkz@gmail.com>
2020-09-26 13:27:06 -0700

    adds exception handling for non-integer in collatz

--- automate_boring_stuff/chapter_3/collatz.py
+++ automate_boring_stuff/chapter_3/collatz.py
@@ -1,15 +1,23 @@
 # Collatz program
 
 def collatz(number):
-    if number <= 0:
-         return 0
-    while (number > 1):
-        if number % 2 == 0:
-            number = int(number / 2)
-        else:
-            number = number * 3 + 1
-        print(number)
+    try:
+        number = int(number)
+        if number <= 0:
+            print(0)
+        while (number > 1):
+            if number % 2 == 0:
+                number = int(number / 2)
+            else:
+                number = number * 3 + 1
+            print(number)
+        return True
+    except ValueError:
+        print("Invalid input")
+        return False
 
-print("Enter number:")
-answer = input()
-collatz(int(answer))
+finished = False
+while (not finished):
+    print("Enter number:")
+    answer = input()
+    finished = collatz(answer)
