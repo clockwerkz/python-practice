0d7bfa21021a3c78ad306185baba6d5457c080ee
clockwerkz <clockwerkz@gmail.com>
2020-10-06 21:56:16 -0700

    finished mclip project

--- automate_boring_stuff/chapter_6/mclip.py
+++ automate_boring_stuff/chapter_6/mclip.py
@@ -1,8 +1,19 @@
 #! python
-# mclip.py
+# mclip.py - A multi-clipboard program.
 import pyperclip
+import sys
 
-pyperclip.copy("Saved to the clipboard!")
+TEXT = {'agree': """Yes, I agree. That sounds fine to me.""",
+'busy': """Sorry, can we do this later this week or next week?""",
+'upsell':"""Would you consider making this a monthly donation?"""}
 
-print("Test")
-input()
+if len(sys.argv) < 2:
+    print('Usage: python mclip.py [keyphrase] - copy phrase text')
+    sys.exit()
+
+keyphrase = sys.argv[1]
+if keyphrase not in TEXT:
+    print('There is no text for ' + keyphrase)
+    sys.exit()
+pyperclip.copy(TEXT[keyphrase])
+print(f'Text for {keyphrase} copied to the clipboard.')
